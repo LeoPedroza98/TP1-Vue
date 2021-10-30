3d14b6ee5d7b401880de95e056b30de3bc148c73
Leonardo Pedroza <leonardopedrozaa@gmail.com>
2021-10-30 20:29:26 -0300

    TP1 finalizado

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <app-header msg="Cadastro TP1"/>
+    <cadastro-descricao />
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import CadastroDescricao from './components/CadastroDescricao.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    CadastroDescricao
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CadastroDescricao.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container" v-bind:key="cadastro.id" v-for="cadastro in cadastros">
+        <p>
+            ID:{{cadastro.id}}
+        </p>
+        <br> 
+        <p>
+            Descrição:{{cadastro.descrição}}
+        </p>
+        <p v-if="cadastro.estaCompleta" id="completo">
+            Cadastro está concluido!!
+        </p>
+        <p v-else id ="inCompleto">
+            Erro:Cadastro Incompleto.. Tente novamente!!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            cadastros:[
+            {id: 1, descricao: "Descrição 111111", estaCompleta: true},
+            {id: 2, descricao: "Descrição 222222", estaCompleta: false},
+            {id: 3, descricao: "Descrição 333333", estaCompleta: false},
+            {id: 4, descricao: "Descrição 4444444", estaCompleta: true},
+            {id: 5, descricao: "Descrição 5555555", estaCompleta: true},
+            {id: 6, descricao: "Descrição 666666", estaCompleta: false}
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped>
+.container{
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  flex-wrap: wrap;
+  width: 300px;
+  height: 250px;
+  text-align: center;
+}
+#completo{
+  color: green;
+  font-weight: bolder;
+  font-size: 1.1em;
+  text-align: center;
+}
+#inCompleto{
+  color: orange;
+  font-weight: bolder;
+  font-size: 1.1em;
+  text-align: center;
+}
+p{
+    text-align: center;
+    
+}
+</style>
